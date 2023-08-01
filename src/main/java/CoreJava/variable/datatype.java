a5948ff8b4fee94c552c3ff9892d1d04aa49c7c0
Annamalai1995 <s.annamalai1995@gmail.com>
2023-08-01 17:46:37 +0530

    Datatype

---
+++ src/main/java/CoreJava/variable/datatype.java
@@ -0,0 +1,20 @@
+package CoreJava.variable;
+
+public class datatype {
+    public static void main(String[] args) {
+        int a=10;
+        double b=5.6666666;
+        float c=535.5f;
+        String name="Tamil";
+        char d='A';
+        Long l=7894561236L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+        System.out.println(d);
+        System.out.println(l);
+
+
+    }
+}
